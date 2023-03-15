50f1066f486ca1ce41fc48f4752a99fc9ce36f83
Thai Tran <tranhoangthai2001@gmail.com>
2023-03-16 02:42:43 +0700

    add widget_utils.dart

---
+++ lib/utils/widget_utils.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/cupertino.dart';
+
+class WidgetUtils {
+  static SizedBox vSpace10() {
+    return const SizedBox(
+      height: 10
+    );
+  }
+  static SizedBox vSpace(double height) {
+    return SizedBox(
+      height: height
+    );
+  }
+}
